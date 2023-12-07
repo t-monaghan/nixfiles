5887419777b2cb1e687574a593aceba7f19afdab
t-monaghan <tomaghan@gmail.com>
2023-12-07 22:15:27 +1100

    adds autocomplete for zsh in rc

--- hosts/culture-amp.nix
+++ hosts/culture-amp.nix
@@ -8,6 +8,6 @@
                               export DIRENV_BIN="/Users/tom.monaghan/.nix-profile/bin/direnv"
                               eval "$($DIRENV_BIN hook zsh)"
                               export NIX_SSL_CERT_FILE='/Library/Application Support/Netskope/STAgent/data/nscacert_combined.pem\'
-                              '';
+                              fpath+=(/Users/tom.monaghan/.nix-profile/share/zsh/site-functions)'';
 };
 }
--- hosts/personal.nix
+++ hosts/personal.nix
@@ -4,5 +4,6 @@
     username = "tmonaghan";
     homeDirectory = "/Users/tmonaghan";
 };
+    programs.zsh.initExtra = "fpath+=(/Users/tmonaghan/.nix-profile/share/zsh/site-functions)";
 }
 
