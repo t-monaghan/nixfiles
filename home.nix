c84bf5d991badb5a53dfb1cedf0fbf004d241374
t-monaghan <tom.monaghan@cultureamp.com>
2024-03-19 11:30:26 +1100

    tidying up, and edit line

--- home.nix
+++ home.nix
@@ -182,8 +182,14 @@
       ];
 
       loginShellInit = ''
+        bind \cx\ce edit_command_buffer
+
         if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
           fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+        end
+            
+        if test -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+          fenv source /nix/var/nix/profiles/default/etc/profile.d/nix.sh
         end'';
     };
 
