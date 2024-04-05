389901847b6029cf70f10df9b69b4b9c7d1171c9
t-monaghan <tom.monaghan@cultureamp.com>
2024-04-05 16:28:46 +1100

    borders: uses hidpi

--- modules/home.nix
+++ modules/home.nix
@@ -13,7 +13,7 @@
     config = rec {
       Label = "com.felixkratz.jankyborders";
       Program = "/etc/profiles/per-user/${username}/bin/borders";
-      ProgramArguments = [ Program "width=8" "active_color=0xffcff1bf" ];
+      ProgramArguments = [ Program "width=8" "active_color=0xffcff1bf hidpi=on" ];
       RunAtLoad = true;
     };
   };
