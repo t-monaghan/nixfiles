60ed5ac0c08af24949f15d3d9e7a0aa5ea09fdb2
t-monaghan <tom.monaghan@cultureamp.com>
2024-06-19 11:18:28 +1000

    fish(fn): clone cd's into repo when unambiguous

--- hosts/culture-amp.nix
+++ hosts/culture-amp.nix
@@ -21,6 +21,7 @@
                   git clone git@github.com:cultureamp/"$argv[1]" "$argv[2..-1]"
                else
                   git clone git@github.com:cultureamp/"$argv[1]"
+                  cd "$argv[1]"
                end'';
     };
   };
