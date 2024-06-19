3d9d1225bf5287d7fe913c0e9e1877e09ad14b96
t-monaghan <tom.monaghan@cultureamp.com>
2024-06-19 11:19:48 +1000

    fish(init): stop sourcing homebrew

--- modules/fish-init.nix
+++ modules/fish-init.nix
@@ -1,6 +1,5 @@
 ''
   set -gx fish_color_autosuggestion brblue
-  eval "$(/opt/homebrew/bin/brew shellenv)"
   bind \cx\ce edit_command_buffer
 
   if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
