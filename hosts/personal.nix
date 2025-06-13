5a36d46e81af2b74fa9fc4bc0cea5efdad20fba8
t-monaghan <tomaghan@gmail.com>
2025-06-13 10:04:29 +0900

    configures direnv to not print env diff

--- hosts/personal.nix
+++ hosts/personal.nix
@@ -11,9 +11,14 @@
     ];
   };
 
-  # TODO: could be good to config around the ENV print on reload
+  # WARN: do not raise this to the shared homemanager config
+  # as the work machine should not have direnv installed by nix
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    config.global = {
+      hide_env_diff = true;
+      warn_timeout = "1h";
+    };
   };
 }
