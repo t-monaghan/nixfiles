b38fd13c08531eebba4908eec9755b3b56403585
t-monaghan <tom.monaghan@cultureamp.com>
2025-07-31 13:55:33 +1000

    organises package file for legibility

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,28 +1,32 @@
 {pkgs}:
 with pkgs; [
+  udev-gothic-nf # great font
+  # cli tools
+  tree
+  killport
+  fzf
+  fblog
+  hyperfine
+  difftastic
+  # linters
+  golangci-lint
+  shellcheck
+  # formatters
+  stylua
+  nixpkgs-fmt
+  alejandra
+  # lsps
+  gopls
+  nil
+  gopls
+  taplo
+  nixd
+  marksman
+  yaml-language-server
+  lua-language-server
   nodePackages_latest.bash-language-server
   nodePackages_latest.typescript-language-server
   nodePackages_latest.vscode-langservers-extracted
   nodePackages_latest.prettier
-  nixd
-  tree
-  yaml-language-server
-  shellcheck
-  taplo
-  killport
-  fzf
-  nixpkgs-fmt
-  marksman
-  gopls
-  udev-gothic-nf
-  fblog
   kotlin-language-server
-  hyperfine
-  difftastic
-  golangci-lint
-  stylua
-  lua-language-server
-  alejandra
-  nil
-  gopls
 ]
