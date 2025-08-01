b0569dcc6ada3b3d83142bbce4bd9548c3ad7a6c
t-monaghan <tom.monaghan@cultureamp.com>
2025-08-01 11:26:30 +1000

    removes references to nix-darwin

--- flake.nix
+++ flake.nix
@@ -3,78 +3,67 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     mac-app-util.url = "github:hraban/mac-app-util";
   };
 
-  outputs =
-    { self
-    , nix-darwin
-    , nixpkgs
-    , home-manager
-    , mac-app-util
-    ,
-    }:
-    {
-
-      homeConfigurations.work =
-        let
-          username = "tom.monaghan";
-        in
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "aarch64-darwin";
-            username = username;
-            config.allowUnfree = true;
-          };
-          modules =
-            [
-              mac-app-util.homeManagerModules.default
-              ./hosts/culture-amp.nix
-            ];
-          extraSpecialArgs = {
-            inherit username;
-          };
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    mac-app-util,
+  }: {
+    homeConfigurations.work = let
+      username = "tom.monaghan";
+    in
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          username = username;
+          config.allowUnfree = true;
         };
-
-      homeConfigurations.personal =
-        let
-          username = "tmonaghan";
-        in
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "aarch64-darwin";
-            username = username;
-            config.allowUnfree = true;
-          };
-          modules =
-            [
-              ./hosts/personal.nix
-            ];
-          extraSpecialArgs = {
-            inherit username;
-          };
+        modules = [
+          mac-app-util.homeManagerModules.default
+          ./hosts/culture-amp.nix
+        ];
+        extraSpecialArgs = {
+          inherit username;
         };
+      };
 
-      homeConfigurations.work-vm =
-        let
-          username = "alanturing";
-        in
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "aarch64-darwin";
-            config.allowUnfree = true;
-          };
-          modules = [
-            ./modules/home.nix
-            ./hosts/work-vm.nix
-          ];
-          extraSpecialArgs = {
-            inherit username;
-          };
+    homeConfigurations.personal = let
+      username = "tmonaghan";
+    in
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          username = username;
+          config.allowUnfree = true;
         };
-    };
+        modules = [
+          ./hosts/personal.nix
+        ];
+        extraSpecialArgs = {
+          inherit username;
+        };
+      };
+
+    homeConfigurations.work-vm = let
+      username = "alanturing";
+    in
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          config.allowUnfree = true;
+        };
+        modules = [
+          ./modules/home.nix
+          ./hosts/work-vm.nix
+        ];
+        extraSpecialArgs = {
+          inherit username;
+        };
+      };
+  };
 }
