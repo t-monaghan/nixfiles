4e2e487dfbc42ce320e85f8a8a1fbb52aa3c3dd6
t-monaghan <tom.monaghan@cultureamp.com>
2025-08-21 09:31:38 +1000

    cleans up store when home-manager removes generations

--- modules/home.nix
+++ modules/home.nix
@@ -28,7 +28,11 @@
   xdg.configFile."fish/completions/nix.fish".source = "${pkgs.nix}/share/fish/vendor_completions.d/nix.fish";
 
   services = {
-    home-manager.autoExpire.enable = true;
+    home-manager.autoExpire = {
+      enable = true;
+      store.cleanup = true;
+    };
+
     jankyborders = {
       enable = true;
       settings = {
