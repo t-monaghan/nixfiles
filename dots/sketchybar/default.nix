57a6aa5491fe8f78fa851d22e669fec0161c0401
t-monaghan <tom.monaghan@cultureamp.com>
2025-09-18 15:06:25 +1000

    combines all sketchybar flags into one call

--- dots/sketchybar/default.nix
+++ dots/sketchybar/default.nix
@@ -54,8 +54,6 @@ in {
         margin=5
       )
 
-      sketchybar --bar "''${bar[@]}"
-
       ##### Changing Defaults #####
       # We now change some default values, which are applied to all further items.
       # For a full list of all available item properties see:
@@ -73,58 +71,37 @@ in {
         label.padding_left=4
         label.padding_right=4
       )
-      sketchybar --default "''${default[@]}"
 
-      ##### Adding Mission Control Space Indicators #####
-      # Let's add some mission control spaces:
-      # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
-      # to indicate active and available mission control spaces.
-
-      sketchybar --add event aerospace_workspace_change
+      workspace_flags=()
       for sid in $(${pkgs.aerospace}/bin/aerospace list-workspaces --all); do
-        sketchybar \
-          --add item space.$sid left \
-          --subscribe space.$sid aerospace_workspace_change \
-          --set space.$sid \
-          background.color=0x44ffffff \
-          background.corner_radius=5 \
-          background.height=20 \
-          background.drawing=off \
-          label="$sid" \
-          click_script="${pkgs.aerospace}/bin/aerospace workspace $sid" \
+        workspace_flags+=(
+          --add item space.$sid left
+          --subscribe space.$sid aerospace_workspace_change
+          --set space.$sid
+          background.color=0x44ffffff
+          background.corner_radius=5
+          background.height=20
+          background.drawing=off
+          label="$sid"
+          click_script="${pkgs.aerospace}/bin/aerospace workspace $sid"
           script="$PLUGIN_DIR/aerospace.sh $sid"
+        )
       done
 
-
-      ##### Adding Left Items #####
-      # We add some regular items to the left side of the bar, where
-      # only the properties deviating from the current defaults need to be set
-
-      sketchybar --add item chevron left \
+      sketchybar --bar "''${bar[@]}" --default "''${default[@]}" --add event aerospace_workspace_change "''${workspace_flags[@]}" \
+                 --add item chevron left \
                  --set chevron icon= label.drawing=off \
                  --add item front_app left \
                  --set front_app icon.drawing=off script="$PLUGIN_DIR/front_app.sh" \
-                 --subscribe front_app front_app_switched
-
-      ##### Adding Right Items #####
-      # In the same way as the left items we can add items to the right side.
-      # Additional position (e.g. center) are available, see:
-      # https://felixkratz.github.io/SketchyBar/config/items#adding-items-to-sketchybar
-
-      # Some items refresh on a fixed cycle, e.g. the clock runs its script once
-      # every 10s. Other items respond to events they subscribe to, e.g. the
-      # volume.sh script is only executed once an actual change in system audio
-      # volume is registered. More info about the event system can be found here:
-      # https://felixkratz.github.io/SketchyBar/config/events
-
-      sketchybar --add item clock right \
+                 --subscribe front_app front_app_switched \
+                 --add item clock right \
                  --set clock update_freq=10 icon=  script="$PLUGIN_DIR/clock.sh" \
                  --add item volume right \
                  --set volume script="$PLUGIN_DIR/volume.sh" \
                  --subscribe volume volume_change \
                  --add item battery right \
                  --set battery update_freq=120 script="$PLUGIN_DIR/battery.sh" \
-                 --subscribe battery system_woke power_source_change
+
 
       ##### Force all scripts to run the first time (never do this in a script) #####
       sketchybar --update
