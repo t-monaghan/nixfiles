c3d516e6fbbbc790ed3df1f544474bd4cedb9f4c
t-monaghan <tomaghan+git@gmail.com>
2026-01-27 10:47:26 +1100

    styles tmux status bar

--- modules/home.nix
+++ modules/home.nix
@@ -104,6 +104,13 @@
       historyLimit = 50000;
       terminal = "screen-256color";
       extraConfig = ''
+        set -g status on
+        set -g status-style bg=default
+        set -g status-left ""
+        set -g status-right "#[fg=green]#S"
+        set -g status-right-length 50
+        set -g window-status-current-format ""
+        set -g window-status-format ""
         bind -Tcopy-mode WheelUpPane send -N 0.25 -X scroll-up
         bind -Tcopy-mode WheelDownPane send -N 0.25 -X scroll-down'';
     };
