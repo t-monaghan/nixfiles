5e577893d5d447dbde6789c83170d0f27799b101
t-monaghan <tomaghan+git@gmail.com>
2026-04-08 15:57:23 +1000

    styles tmux

--- modules/programs/tmux/home.nix
+++ modules/programs/tmux/home.nix
@@ -13,14 +13,14 @@ lib.mkIf config.nixfiles.programs.tmux.enable {
     historyLimit = 50000;
     terminal = "screen-256color";
     extraConfig = ''
-      set -g status on
-      set -g status-style bg=default
-      set -g status-left ""
-      set -g status-right "#[fg=green]#S"
-      set -g status-right-length 50
-      set -g window-status-current-format ""
-      set -g window-status-format ""
+      set -g status off
+      set -g pane-border-status top
+      set -g pane-border-format ' #{?#{==:#{pane_current_command},fish},#{session_name},#{pane_title}}#{?#{==:#{pane_index},0},#[align=right]#{S:#{?session_attached,#[fg=green]─ #{session_name}#{?#{>:#{session_windows},1}, #{e|+:#{active_window_index},1}|#{session_windows},} #[default],#[dim]─ #{session_name}#{?#{>:#{session_windows},1}, #{e|+:#{active_window_index},1}/#{session_windows},} #[default]}}──,}'
       bind -Tcopy-mode WheelUpPane send -N 0.25 -X scroll-up
-      bind -Tcopy-mode WheelDownPane send -N 0.25 -X scroll-down'';
+      bind -Tcopy-mode WheelDownPane send -N 0.25 -X scroll-down
+
+      # Highlight active pane background when prefix is pressed
+      bind -Troot C-b select-pane -P 'bg=colour235' \; switch-client -Tprefix \; run -b 'sleep 1 && tmux select-pane -P bg=default'
+    '';
   };
 }
