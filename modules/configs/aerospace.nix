44994ced1eb1e40c57a3a513ce210e855ab4c1d4
t-monaghan <tomaghan+git@gmail.com>
2026-05-07 14:41:41 +1000

    aerospace: alt+tab now switches to last focused node, not window

--- modules/configs/aerospace.nix
+++ modules/configs/aerospace.nix
@@ -58,7 +58,7 @@
     "alt-shift-equal" = "resize smart +200";
 
     "cmd-tab" = "move-node-to-monitor --wrap-around next";
-    "alt-tab" = "workspace-back-and-forth";
+    "alt-tab" = "focus-back-and-forth";
     "alt-shift-tab" = "move-workspace-to-monitor --wrap-around next";
   };
   on-window-detected = [
