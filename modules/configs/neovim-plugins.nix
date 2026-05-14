446702792b3e52e90f789b91dbe37fc1cd0145a3
t-monaghan <tomaghan+git@gmail.com>
2026-05-14 13:13:45 +1000

    adds neovim plugin flash

--- modules/configs/neovim-plugins.nix
+++ modules/configs/neovim-plugins.nix
@@ -13,6 +13,11 @@
       };
     };
 
+    flash = {
+      enable = true;
+      settings = {};
+    };
+
     gitsigns = {
       enable = true;
       settings = {
@@ -274,6 +279,36 @@
 
   # Extra telescope keymaps that need raw Lua (custom picker functions)
   keymaps = [
+    {
+      mode = ["n" "x" "o"];
+      key = "s";
+      action.__raw = ''function() require("flash").jump() end'';
+      options.desc = "Flash";
+    }
+    {
+      mode = ["n" "x" "o"];
+      key = "S";
+      action.__raw = ''function() require("flash").treesitter() end'';
+      options.desc = "Flash Treesitter";
+    }
+    {
+      mode = "o";
+      key = "r";
+      action.__raw = ''function() require("flash").remote() end'';
+      options.desc = "Remote Flash";
+    }
+    {
+      mode = ["o" "x"];
+      key = "R";
+      action.__raw = ''function() require("flash").treesitter_search() end'';
+      options.desc = "Treesitter Search";
+    }
+    {
+      mode = "c";
+      key = "<c-s>";
+      action.__raw = ''function() require("flash").toggle() end'';
+      options.desc = "Toggle Flash Search";
+    }
     {
       mode = "n";
       key = "<leader>/";
