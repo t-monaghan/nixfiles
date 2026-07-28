88c465dcc409f916aafca7466815d302a9b64ffb
t-monaghan <tomaghan@gmail.com>
2026-07-28 13:03:58 +1000

    configures adguard for nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -109,6 +109,18 @@
         PermitRootLogin = "no";
       };
     };
+    adguardhome = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        dns = {
+          upstream_dns = [
+            "1.1.1.1"
+            "8.8.8.8"
+          ];
+        };
+      };
+    };
   };
 
   fileSystems."/media" = {
