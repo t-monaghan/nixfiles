90543ce18e90a3257960361e7a09ba9174997ce2
t-monaghan <tomaghan@gmail.com>
2026-07-28 13:17:02 +1000

    adds mullvad resolver as fallback dns

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -115,8 +115,7 @@
       settings = {
         dns = {
           upstream_dns = [
-            "1.1.1.1"
-            "8.8.8.8"
+            "10.64.0.1" # mullvads in tunnel dns resolver
           ];
         };
       };
