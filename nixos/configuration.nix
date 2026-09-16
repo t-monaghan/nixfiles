533894b2a1a46b3d1c12904e7666dd9084e3f359
t-monaghan <tomaghan@gmail.com>
2026-09-16 15:41:58 +1000

    sets up nixos gc

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,10 +9,23 @@
     ./modules/home-assistant.nix
   ];
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix = {
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
 
   boot.loader = {
-    systemd-boot.enable = true;
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 5;
+    };
     efi.canTouchEfiVariables = true;
   };
 
