da3646dc54676e76c9db13bacb7ea96a660e23d3
t-monaghan <tomaghan+git@gmail.com>
2026-09-24 09:29:31 +1000

    bumps nixpkgs to branch with new sesh

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Tom Monaghan's flake for system configuration across machines";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:t-monaghan/nixpkgs/bumps-pi-coding-agent-0-87-1";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixvim.url = "github:nix-community/nixvim";
